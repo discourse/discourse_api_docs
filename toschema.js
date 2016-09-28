c5ff2955da3abbafad80d1e29ca6886f1b3f69cb
Blake Erickson <o.blakeerickson@gmail.com>
2016-09-28 08:47:52 -0600

    add type null to empty objects

--- toschema.js
+++ toschema.js
@@ -12,15 +12,30 @@ if (process.argv.length > 3) {
   console.log('node schema.js input.json output.yml');
 }
 
+var addTypeIfEmptyObject = function(obj) {
+  for(var i in obj) {
+    if (typeof obj[i] === "object") {
+      if (Object.keys(obj[i]).length === 0) {
+        obj[i] = {type: null};
+      } else {
+        addTypeIfEmptyObject(obj[i]);
+      }
+    }
+  }
+  return null;
+}
+
 fs.readFile(inputFile, 'utf8', function(err, data) {
   if (err) throw err;
   var json = JSON.parse(data);
   var schema = gen(json);
   delete schema.required;
 
-  //TODO: redoc doesn't like the yml when `inline > 2` (2nd argument in stringify command)
-  //apparently this is because there are some properties without types on them(they should just be set to `null`)
-  var yml = yaml.stringify(schema, 2, 2);
+
+  //there are some properties without types on them(they should just be set to `null`)
+  addTypeIfEmptyObject(schema.properties);
+
+  var yml = yaml.stringify(schema, 16, 2);
 
   fs.writeFile(outputFile, yml, function(err) {
     if (err) return console.log(err);
